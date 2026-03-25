5c99aaabbc3929a7317cde3081c521b93c288923
Łukasz Darmofał <legolokskoks@gmail.com>
2026-03-25 13:00:19 +0100

    Create Vehicle class

--- FleetManager/Models/Vehicle.cs
+++ FleetManager/Models/Vehicle.cs
@@ -2,5 +2,8 @@ namespace FleetManager.Models;
 
 public class Vehicle
 {
-    
+    public string Name = "";
+    public string LicensePlate = "";
+    public int FuelLevel = 100;
+    public string Status = "Available"; 
 }
